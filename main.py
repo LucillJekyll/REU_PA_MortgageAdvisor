215a9bef69a4d6f06d465a8055de583bcea290a4
Your Name <you@example.com>
2021-12-14 15:33:59 +0300

    testing git

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print('test')
+import sys
+from PA_project_menu import Ui_MainWindow
+from PyQt5 import QtWidgets, QtCore
+
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+
+if __name__ == '__main__':
+    import sys
+
+    app = QtWidgets.QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
